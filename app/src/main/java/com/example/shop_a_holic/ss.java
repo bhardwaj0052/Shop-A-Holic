06fdd75230594ee1eb178bdff0ad79a56b348be8
bhardwaj0052 <rahulsharma7618@gmail.com>
2023-10-27 15:03:41 +0530

    first commit

---
+++ app/src/main/java/com/example/shop_a_holic/ss.java
@@ -0,0 +1,14 @@
+package com.example.shop_a_holic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ss extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ss);
+    }
+}
